b0f4eea62a2eedebfe7f044341fb9f7f86509c04
Maykon Mori <mjose-ye@student.42sp.org.br>
2022-06-18 14:36:47 -0300

    alterando verificação de parede

--- src/check_walls.c
+++ src/check_walls.c
@@ -1,5 +1,57 @@
 #include "cub3D.h"
 
+void	check_diagonal_up_right(t_data *data, int y, int x)
+{
+	while (y >= 0 || x < data->map.x_size)
+	{
+		if (data->map.map[y][x] == '1')
+			return ;
+		y--;
+		x++;
+	}
+	printf ("Error\n mapa nao rodeado por 1\n");
+	exit_click(data);
+}
+
+void	check_diagonal_up_left(t_data *data, int y, int x)
+{
+	while (y >= 0 || x >= 0)
+	{
+		if (data->map.map[y][x] == '1')
+			return ;
+		y--;
+		x--;
+	}
+	printf ("Error\n mapa nao rodeado por 1\n");
+	exit_click(data);
+}
+
+void	check_diagonal_down_left(t_data *data, int y, int x)
+{
+	while (x >= 0 || y < data->map.y_size)
+	{
+		if (data->map.map[y][x] == '1')
+			return ;
+		x--;
+		y++;
+	}
+	printf ("Error\n mapa nao rodeado por 1\n");
+	exit_click(data);
+}
+
+void	check_diagonal_down_right(t_data *data, int y, int x)
+{
+	while (x < data->map.x_size || y < data->map.y_size)
+	{
+		if (data->map.map[y][x] == '1')
+			return ;
+		x++;
+		y++;
+	}
+	printf ("Error\n mapa nao rodeado por 1\n");
+	exit_click(data);
+}
+
 void	check_left(t_data *data, int y, int x)
 {
 	while (x >= 0)
@@ -36,6 +88,7 @@ void	check_down(t_data *data, int y, int x)
 	exit_click(data);
 }
 
+
 void	check_up(t_data *data, int y, int x)
 {
 	while (y >= 0)
@@ -59,12 +112,16 @@ void	check_walls(t_data *data)
 		x = -1;
 		while (data->map.map[y][++x])
 		{
-			if (data->map.map[y][x] == '0')
+			if (data->map.map[y][x] == '0' || data->map.map[y][x] == 'N' || data->map.map[y][x] == 'S' || data->map.map[y][x] == 'W' || data->map.map[y][x] == 'E')
 			{
 				check_up(data, y, x);
 				check_left(data, y, x);
 				check_right(data, y, x);
 				check_down(data, y, x);
+				check_diagonal_up_right(data, y, x);
+				check_diagonal_up_left(data, y, x);
+				check_diagonal_down_right(data, y, x);
+				check_diagonal_down_left(data, y, x);
 			}
 		}
 	}
